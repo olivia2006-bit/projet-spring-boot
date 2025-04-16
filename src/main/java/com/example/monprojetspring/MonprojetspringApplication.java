c47c861f691bc540a4b679b24cb9242c20df35cc
olivia2006-bit <abuiolive@gmail.com>
2025-04-16 14:26:33 +0100

    initial commit

---
+++ src/main/java/com/example/monprojetspring/MonprojetspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.monprojetspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonprojetspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonprojetspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/monprojetspring/MonprojetspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monprojetspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonprojetspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
